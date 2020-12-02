58453c463504ba49dd4aeb03bf5ee7b9d4c05a28
catd825 <catd825@gmail.com>
2020-12-02 18:19:17 -0500

    added keys

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 //gives us access to the express library
 
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express();
 //typically only has one app, but can have many
 
-app.get('/', (req,res) => {
-    res.send({ bye: 'buddy'});
-});
+passport.use(new GoogleStrategy());
+//new GoogleStrategy() creates a new instance of the passport strategy
+//passport.use asks passport to be aware that they can use Google Strategy to authenticate
+
 
 const PORT = process.env.PORT || 5000;
 //look at underlying environment to see what port Heroku wants us to use, otherwise default to 5000
 
 app.listen(PORT);
+
+
+
+
+// app.get('/', (req,res) => {
+//     res.send({ bye: 'buddy'});
+// });
