8a67ca57573eaecbf20ec775a80d91160301bf51
catd825 <catd825@gmail.com>
2020-12-07 17:24:21 -0500

    added callback endpoint

--- index.js
+++ index.js
@@ -3,13 +3,43 @@ const express = require('express');
 
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
 const app = express();
 //typically only has one app, but can have many
 
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            console.log('access token', accessToken);
+            console.log('refreshToken', refreshToken);
+            console.log('profile', profile);
+        }
+    )
+);
 //new GoogleStrategy() creates a new instance of the passport strategy
 //passport.use asks passport to be aware that they can use Google Strategy to authenticate
+//GoogleStrategy is known as a string called google when passport loads it up - this is what is called below in authentication route
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+//when they come to the route, they are directed into the oauth flow managed by passport.
+//scope is based on what google has access to (emails, contact list)
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
+
 
 
 const PORT = process.env.PORT || 5000;
