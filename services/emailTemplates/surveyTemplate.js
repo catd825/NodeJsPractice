39cb6e83859203c738cdbd07b97f9997b51ed15c
catd825 <catd825@gmail.com>
2021-01-27 14:37:20 -0500

    added credit reduction

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,17 @@
-module.exports = (survey) => {
-    return '<div>' + survey.body + '</div>';
+module.exports = survey => {
+    return `
+    <html>
+        <body>
+            <div>
+                <h3 style="text-align: center;">I'd like your input</h3>
+                <p>Please answer the following question:</p>
+                <p>${survey.body}</p>
+                <div>
+                    <a href="http://localhost:3000">Yes</a>
+                    <a href="http://localhost:3000">No</a>
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
 }
