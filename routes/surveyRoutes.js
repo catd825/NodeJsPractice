2e7939d23352cc850c73bc0a32414a2b1b61ee2e
catd825 <catd825@gmail.com>
2021-01-27 13:35:55 -0500

    updated sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
-const Mailer = require('..services/Mailer')
+const Mailer = require('../services/Mailer')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys')
@@ -14,7 +14,7 @@ module.exports = app => {
         const survey = new Survey({
             title, //equivalent to title: title
             subject,
-            body,
+            body, 
             // recipients: recipients.split(',').map(email => { return {email: email }})
             // recipients: recipients.split(',').map(email => ({ email } ))
             recipients: recipients.split(',').map(email => ({ email: email.trim() })),
@@ -22,6 +22,6 @@ module.exports = app => {
             dateSent: Date.now()
         })
         const mailer = new Mailer(survey, surveyTemplate(survey));
-
+        mailer.send();
     })
 }
