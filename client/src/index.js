c24c5cb8d788d695656e70bf73e373ed31322dd5
catd825 <catd825@gmail.com>
2021-01-28 12:21:37 -0500

    updated forms

--- client/src/index.js
+++ client/src/index.js
@@ -19,5 +19,5 @@ ReactDOM.render(
     document.querySelector('#root')
 );
 
-console.log('stripe key is', process.env.REACT_APP_STRIPE_KEY)
-console.log('environment is', process.env.NODE_ENV)
+// console.log('stripe key is', process.env.REACT_APP_STRIPE_KEY)
+// console.log('environment is', process.env.NODE_ENV)
